eb70fb8b658c515296a34fa845b96f043d84eeff
post-coder <gabrielspanu.dev@gmail.com>
2024-02-14 12:15:32 +0100

    snack 1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,93 @@
-
-// hai un array di numeri da 1 a 5
-// crea un nuovo array con i solo i numeri dispari
-
-const numbers = [1,2,3,4,5];
-// [1,3,5]
-
-// array nuovo vuoto
-let newArray = [];
-
-
-// possiamo utilizzare il nuovo metodo .filter()
-// cicla tutti gli elementi dell'array di partenza
-// si aspetta di trovare da qualche parte per ogni elemento un "return true"
-// se si trova un return true, inserisce QUELL'ELEMENTO attuale nel nuovo array
-
-newArray = numbers.filter((currentNumber) => {
-    
-    if(currentNumber % 2 != 0) {
-        return true;
+var automobili = [
+    { 
+        marca: "Fiat", 
+        modello: "500", 
+        alimentazione: "benzina"
+    },
+    { 
+        marca: "Volkswagen", 
+        modello: "Golf", 
+        alimentazione: "diesel"
+    },
+    { 
+        marca: "Tesla", 
+        modello: "Model S", 
+        alimentazione: "elettrico"
+    },
+    { 
+        marca: "Toyota", 
+        modello: "Prius", 
+        alimentazione: "ibrida"
+    },
+    { 
+        marca: "Renault", 
+        modello: "Clio", 
+        alimentazione: "gpl"
+    },
+    { 
+        marca: "BMW", 
+        modello: "i3", 
+        alimentazione: "elettrico"
+    },
+    { 
+        marca: "Audi", 
+        modello: "A3", 
+        alimentazione: "diesel"
+    },
+    { 
+        marca: "Hyundai", 
+        modello: "Kona", 
+        alimentazione: "elettrico"
+    },
+    { marca: 
+        "Mercedes-Benz", 
+        modello: "GLC", 
+        alimentazione: "diesel"
+    },
+    { 
+        marca: "Fiat", 
+        modello: "Panda", 
+        alimentazione: "metano"
     }
+];
 
-    // metodo più veloce:
-    // return currentNumber % 2 != 0
+/*
+Dividi le automobili in 3 array separati: 
+nel primo array solo le auto a benzina, 
+nel secondo solo le auto a diesel,
+ nel terzo il resto delle auto.
+ */
 
+// array di macchine a benzina
+let benzinCars = [];
+
+// metodo con arrow function
+benzinCars = automobili.filter(currentCar => {
+    // forma contratta per restituire true o false in base alla condizione
+    return currentCar.alimentazione == "benzina";
+
+});
+console.log(benzinCars)
+
+
+
+// array di macchine a diesel
+// qui usiamo il modo tradizionale per la funzione (completamente interscambiabile con la arrow function)
+const dieselCars = automobili.filter(function(currentCar) {
+    // controllo tradizionale
+    if (currentCar.alimentazione == "diesel") {
+        return true
+    }
 })
+console.log(dieselCars)
 
-console.log(newArray)
-// considerazioni:
-// l'array che ci restituirà il metodo .filter() non potrà mai essere con più elementi rispetto a quello di partenza
-// può avere lo stesso numero o inferiore. (di solito inferiore, visto che stiamo filtrando)
+
+
+// array di macchine con diversa alimentazione da benzina e diesel
+// forma più contratta possibile
+// mettiamo in una stessa riga il filter, 
+    // il parametro che richiediamo di volta in volta "actualCar" 
+    // e il valore da restituire (condizione che ci da true o false)
+    // dentro la condizione ricordate che potete utilizzare a piacimento && e ||
+const otherCars = automobili.filter( actualCar => actualCar.alimentazione != 'benzina' && actualCar.alimentazione != "diesel");
+console.log(otherCars)
